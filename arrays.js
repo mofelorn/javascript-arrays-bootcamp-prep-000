aaff4948a50f87b7e9d76202fbdaf1095f3c5ad6
Felix Moajdo Jr <mojadof@yahoo.com>
2019-10-31 18:15:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -38,11 +38,9 @@ function destructivelyRemoveElementFromEndOfArray(array) {
   return array
 }
 
-function removeElementFromEndOfArray(array) {
-  return array.slice(0, array.lenght-1);
+function removeElementFromEndOfArray(array){
+  return array.slice(0, array.length-1);
 }
-
-
 
 
 
